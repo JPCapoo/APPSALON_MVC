627e6cfec555240ab435ea1e7ee7782536ada635
Juan P Capobianco <capobiancoramos@gmail.com>
2022-02-14 16:56:10 -0700

    Deployment de la app

---
+++ classes/Email.php
@@ -0,0 +1,80 @@
+<?php 
+
+namespace Classes;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email{
+
+    public $email;
+    public $nombre;
+    public $token;
+
+    public function __construct($email, $nombre, $token) {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+
+    public function enviarConfirmacion() {
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '83b1b9d0004a6c';
+        $mail->Password = '58b84ab4f7da59';
+
+        $mail->setFrom('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Confirma tu cuenta';
+
+        //Set HTML
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> has creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
+        $contenido .= "</html>";
+
+        //Se agrega el contenido al objeto de mail
+        $mail->Body = $contenido;
+
+        $mail->send();
+    }
+
+    public function enviarInstrucciones(){
+        //Crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '83b1b9d0004a6c';
+        $mail->Password = '58b84ab4f7da59';
+
+        $mail->setFrom('cuentas@appsalon.com');
+        $mail->addAddress('cuentas@appsalon.com', 'AppSalon.com');
+        $mail->Subject = 'Reestablece tu password';
+
+        //Set HTML
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = "<html>";
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
+        $contenido .= "</html>";
+
+        //Se agrega el contenido al objeto de mail
+        $mail->Body = $contenido;
+
+        $mail->send();
+    }
+}
+
+?>
---
+++ controllers/APIController.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Controllers;
+
+use Model\Cita;
+use Model\CitaServicio;
+use Model\Servicio;
+
+class APIController {
+    public static function index() {
+        //Obtiene todos los servicios de la BDD usando el modelo de servicio
+        $servicios = Servicio::all();
+        
+        //Como se llama la función index en api/servicios, se manda el arreglo asociativo de servicios y se convierte a JSON para que lo reciba JS
+        echo json_encode($servicios);
+    }
+
+    public static function guardar() {
+        // Almacena la cita y devuelve el ID
+        $cita = new Cita($_POST);
+
+        $resultado = $cita->guardar();
+
+        $id = $resultado['id'];
+
+        //Almacena los Servicios con el ID de la cita
+        $idServicios = explode(",", $_POST['servicios']);
+        foreach($idServicios as $idServicio){
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+        
+        echo json_encode(['resultado' => $resultado]);
+    }
+
+    public static function eliminar() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $id = $_POST['id'];
+
+            $cita = Cita::find($id);
+            $cita->eliminar();
+
+            header('Location:' . $_SERVER['HTTP_REFERER']);
+        }
+    }
+}
+
+?>
